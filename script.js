ef617e0435bc97e888013a010efb0c5d73d9b8e1
MatthewFarha <m.farha77@quincy.edu>
2024-03-19 01:14:15 -0500

    update

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
 window.addEventListener("load", loadPage);
-var moves = new Array()
-var buttons = new Array(4);
+var maxMoves = 10; /*Maximum moves flag*/
+var moves = new Array(maxMoves)
+var buttons = new Array(4)
+var currentButton = 0;
+/*load page*/
 function loadPage(){
     for(i=0; i<4; i++){
         var str = "#mb" + (i+1)
         buttons[i] = document.querySelector(str)
-        buttons[i].addEventListener("click", memButtonClick)
-        buttons[i].myParam = i;
+    }
+    loadMoves()
+}
+/*load array of moves*/
+function loadMoves(){
+    for(i = 0; i<maxMoves; i++){    
+        var rn = Math.ceil((Math.random()*4-1))
+        if(rn==0)
+            rn=1
+        moves[i] = buttons[rn]
+        console.log(moves[i].innerHTML)
     }
 }
-function newMove(){
-    var rn = Math.ceil((math.random()*4-1))
-    if(rn==0)
-        rn=1
-    moves[currentMove] = buttons[rn]
-}
-function memButtonClick(evt){
-    console.log(evt.currentTarget.myParam)
-}
-function checkButtonComp(){
-    
+/*onclick function that pushes currentButton forward if correct or restart (reload page)*/
+function memButtonClick(x){
+    if(buttons[x] == moves[currentButton]){
+        currentButton++
+        console.log("Correct!")
+    }
+    else
+        location.reload()
 }
