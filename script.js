1906f0fa42e9bd4fdd89d2372d03f35313097e10
MatthewFarha <m.farha77@quincy.edu>
2024-03-26 10:29:38 -0500

    Memory game

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-window.addEventListener("load", loadPage);
+window.addEventListener("load", loadPage); // Wait for page to load to start setting elements
 var maxMoves = 10; /*Maximum moves flag*/
 var moves = new Array(maxMoves)
 var buttons = new Array(4)
 var currentButton = 0;
+var currentSize = 0;
+var blind = false;
+var colors = new Array(4)
 /*load page*/
 function loadPage(){
-    for(i=0; i<4; i++){
-        var str = "#mb" + (i+1)
-        buttons[i] = document.querySelector(str)
+    for(i=1; i<5; i++){
+        var str = "#mb" + (i)
+        buttons[i-1] = document.querySelector(str)
+        colors[i-1] = window.getComputedStyle(buttons[i-1], null).getPropertyValue('background-color')
     }
     loadMoves()
 }
 /*load array of moves*/
 function loadMoves(){
     for(i = 0; i<maxMoves; i++){    
-        var rn = Math.ceil((Math.random()*4-1))
-        if(rn==0)
-            rn=1
+        var rn = Math.ceil((Math.random()*4))-1
         moves[i] = buttons[rn]
         console.log(moves[i].innerHTML)
     }
@@ -29,4 +31,20 @@ function memButtonClick(x){
     }
     else
         location.reload()
+}
+function blindMode(){
+    if(blind){
+        for(i=0; i<4;i++){
+            buttons[i].style.backgroundColor = colors[i]
+            buttons[i].style.color = "#000000"
+        }
+        blind = false
+    }
+    else{
+        for(i=0; i<4;i++){
+            buttons[i].style.backgroundColor = "#555555"
+            buttons[i].style.color = "#FFFFFF"
+        }
+        blind = true
+    }
 }
